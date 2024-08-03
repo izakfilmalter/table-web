fd06dcc736f82b3751d6f74cc2f553c4a75aca1b
Izak Filmalter <izak@churchphone.com>
2024-08-03 13:21:57 -0400

    Start stubbing out navigation along with new home page.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,20 +1,64 @@
-import { Logo } from '@/components/logo'
 import { RichText } from '@/components/richText/richText'
+import { Card } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
 import type { Vision } from '@/payload-types'
 import { getCachedGlobal } from '@/shared/payload/getGlobals'
+
+const contentClassName =
+  'mt-24 w-full max-w-[780px] text-[22px] md:text-3xl font-semibold leading-[1.4] md:leading-[1.5]'
 
 export default async function Home() {
   const vision: Vision = await getCachedGlobal('vision', 1)()
 
   return (
-    <div
-      className={
-        'mx-auto my-16 flex max-w-[732px] flex-col items-start gap-16 px-8'
-      }
-    >
-      <Logo className={'h-auto w-full md:h-32 md:w-auto'} />
+    <>
+      <div
+        className={
+          'animate-fade-in relative m-4 flex h-[calc(100dvh-72px-32px)] max-h-[1024px] translate-y-[-10px] flex-col overflow-hidden rounded-[32px] bg-white bg-[url(/st-pete.webp)] bg-cover opacity-0 md:min-h-[658px] md:rounded-[48px]'
+        }
+      >
+        <div className={'m-auto flex h-full flex-col px-4 py-4'}>
+          <h1
+            className={
+              // 96px is total pixels between each side of the screen and the start
+              // of the text. This makes it so that the hero text is always edge to
+              // edge on mobile.
+              'animate-fade-in my-auto translate-y-[-10px] font-serif text-[min(calc((100vw-96px)/10),80px)] leading-[1.4] text-white opacity-0 delay-200 md:mb-0 md:leading-[1.7]'
+            }
+          >
+            <span
+              className={
+                'rounded-3xl bg-black box-decoration-clone px-4 pb-1.5 pt-4 md:px-8 md:pb-3.5 md:pt-8'
+              }
+            >
+              Discipleship
+              <br />
+              At Scale
+            </span>
+          </h1>
 
-      <RichText content={vision.content} className={'text-4xl font-bold'} />
-    </div>
+          <Card
+            className={
+              'delay-400 animate-fade-in translate-y-[-10px] items-start gap-4 !border-0 bg-white/70 p-4 opacity-0 backdrop-blur-[12px] md:mb-auto md:p-8'
+            }
+          >
+            <h2 className={cn(contentClassName, 'mt-0')}>
+              Equip your team with an app for data driven discipleship.
+              <br />
+              <span className={'hidden md:inline-block'}>
+                Streamlining Next Steps, growing leaders, and more.
+              </span>
+            </h2>
+          </Card>
+        </div>
+      </div>
+      <div
+        className={
+          'mx-auto my-16 flex max-w-[732px] flex-col items-start gap-16 px-8'
+        }
+      >
+        <RichText content={vision.content} className={'text-4xl font-bold'} />
+      </div>
+    </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss'
 import animate from 'tailwindcss-animate'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 const config = {
   darkMode: ['class'],
@@ -18,9 +19,92 @@ const config = {
         '2xl': '1400px',
       },
     },
+    transitionDelay: {
+      ...defaultTheme.transitionDelay,
+      0: '0ms',
+    },
+    keyframes: {
+      ...defaultTheme.keyframes,
+      'fade-in': {
+        from: { opacity: '0', transform: 'translateY(-10px)' },
+        to: { opacity: '1', transform: 'none' },
+      },
+      'image-rotate': {
+        '0%': { transform: 'rotateX(25deg)' },
+        '25%': { transform: 'rotateX(25deg) scale(0.9)' },
+        '60%': { transform: 'none' },
+        '100%': { transform: 'none' },
+      },
+      'image-glow': {
+        '0%': {
+          opacity: '0',
+          'animation-timing-function': 'cubic-bezier(0.74,0.25,0.76,1)',
+        },
+        '10%': {
+          opacity: '1',
+          'animation-timing-function': 'cubic-bezier(0.12,0.01,0.08,0.99)',
+        },
+        '100%': {
+          opacity: '0.2',
+        },
+      },
+      'sketch-lines': {
+        '0%': { 'stroke-dashoffset': '1' },
+        '50%': { 'stroke-dashoffset': '0' },
+        '99%': { 'stroke-dashoffset': '0' },
+        '100%': { visiblity: 'hidden' },
+      },
+      'glow-line-horizontal': {
+        '0%': { opacity: '0', transform: 'translateX(0)' },
+        '5%': { opacity: '1', transform: 'translateX(0)' },
+        '90%': { opacity: '1' },
+        '100%': { opacity: '0', transform: 'translateX(min(60vw, 45rem))' },
+      },
+      'glow-line-vertical': {
+        '0%': { opacity: '0', transform: 'translateY(0)' },
+        '5%': { opacity: '1', transform: 'translateY(0)' },
+        '90%': { opacity: '1' },
+        '100%': { opacity: '0', transform: 'translateY(min(21vw, 45rem))' },
+      },
+      zap: {
+        '0%, 9%, 11%, 100% ': {
+          fill: 'transparent',
+        },
+        '10%': {
+          fill: 'white',
+        },
+      },
+      bounce: {
+        '50%': {
+          transform: 'scale(0.98)',
+        },
+      },
+    },
+    animation: {
+      ...defaultTheme.animation,
+      'fade-in': 'fade-in 1000ms var(--animation-delay, 0ms) ease forwards',
+      'image-rotate': 'image-rotate 1400ms ease forwards',
+      'image-glow': 'image-glow 4100ms 600ms ease-out forwards',
+      'sketch-lines': 'sketch-lines 1200ms ease-out forwards',
+      'glow-line-horizontal':
+        'glow-line-horizontal var(--animation-duration) ease-in forwards',
+      'glow-line-vertical':
+        'glow-line-vertical var(--animation-duration) ease-in forwards',
+      zap: 'zap 2250ms calc(var(--index) * 20ms) linear infinite',
+      bounce: '240ms ease 0s 1 running bounce',
+    },
     extend: {
+      spacing: {
+        'navigation-height': 'var(--navigation-height)',
+      },
       fontFamily: {
         sans: ['var(--font-sans)'],
+        serif: [
+          'var(--font-serif)',
+          {
+            fontVariationSettings: '"wght" 300',
+          },
+        ],
       },
       colors: {
         border: 'hsl(var(--border))',
@@ -75,6 +159,23 @@ const config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        pulse: 'pulse 2s cubic-bezier(.4,0,.6,1) infinite',
+        hide: 'hide 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideDownAndFade:
+          'slideDownAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideLeftAndFade:
+          'slideLeftAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideRightAndFade:
+          'slideRightAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        // Accordion
+        accordionOpen: 'accordionOpen 150ms cubic-bezier(0.87, 0, 0.13, 1)',
+        accordionClose: 'accordionClose 150ms cubic-bezier(0.87, 0, 0.13, 1)',
+        // Dialog
+        dialogOverlayShow:
+          'dialogOverlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        dialogContentShow:
+          'dialogContentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
     },
   },
